0061277055a331d5818b737b25ff581add41b906
ns315522 <nickstrauch205@gmail.com>
2023-09-07 17:03:18 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nicholas Strauch
+ *        Date: 09/07/23
+ * Description: My second CS lab project.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
